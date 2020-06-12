3b643d1e4d9a28b8483d32b3f65411701c68f23a
Cornelius <bcornelius@iseatz.com>
2020-06-12 11:34:57 -0500

    updates polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -74,6 +74,8 @@
  *
  */
 (window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = require('process');
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
