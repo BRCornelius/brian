c626d807f5062bbbb880a318280d37248be7ea68
Cornelius <bcornelius@iseatz.com>
2020-06-26 13:27:38 -0500

    removes unused imports

--- src/app/services/content/otf.service.ts
+++ src/app/services/content/otf.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { IFormattedOTFVideo, IOTFVideo, IOptions } from '../../interfaces';
 
 @Injectable({
