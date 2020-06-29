057b983beabae27cc90eee22485d7f633a0b0964
Cornelius <bcornelius@iseatz.com>
2020-06-29 10:53:00 -0500

    updates test.ts file

--- src/test.ts
+++ src/test.ts
@@ -13,10 +13,10 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-// const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 
 // Then we find a specific test.
-const context = require.context('./', true, /media.service\.spec\.ts$/)
+// const context = require.context('./', true, /media.service\.spec\.ts$/)
 
 // And load the modules.
 context.keys().map(context);
