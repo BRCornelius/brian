019ca2b7f8a63864f2c998ba14eaebe4636030a8
Cornelius <bcornelius@iseatz.com>
2020-07-17 13:59:24 -0500

    updates import of otf pipe

--- src/app/workouts/components/workout-card/workout-card.component.ts
+++ src/app/workouts/components/workout-card/workout-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { OtfUrlPipe } from 'src/app/workouts/pipes/otf-url.pipe';
+import { OtfUrlPipe } from '../../pipes';
 
 @Component({
   selector: 'workout-card',
